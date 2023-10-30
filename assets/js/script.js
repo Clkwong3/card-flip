04062a48efa1132eb9def8f67ed06850eb53b15b
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-10-30 00:33:38 -0700

    JS/script.js - only 2 cards can be clicked at once.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,15 @@
 const resetBtn = document.querySelector(".reset");
 
 const halloweenEmojis = [
-  "🎃",
-  "🎃",
-  "👻",
-  "👻",
-  "🦇",
-  "🦇",
-  "🕷️",
-  "🕷️",
-  "🕸️",
-  "🕸️",
-  "🍬",
-  "🍬",
-  "🕯️",
-  "🕯️",
-  "🧙‍♀️",
-  "🧙‍♀️",
-  "🧛‍♂️",
-  "🧛‍♂️",
-  "🧟‍♂️",
-  "🧟‍♂️",
-  "🦴",
-  "🦴",
-  "🎭",
-  "🎭",
-  "🍁",
-  "🍁",
-  "🌙",
-  "🌙",
+  "🎃", "🎃", "👻", "👻", "🦇", "🦇", "🕷️", "🕷️", "🕸️", "🕸️", "🍬", "🍬",
+  "🕯️", "🕯️", "🧙‍♀️", "🧙‍♀️", "🧛‍♂️", "🧛‍♂️", "🧟‍♂️", "🧟‍♂️", "🦴", "🦴",
+  "🎭", "🎭", "🍁", "🍁", "🌙", "🌙",
 ];
 
-// Shuffle the emojis array
-// More explicit sorting logic based on a random condition to get either 2 or -1
-// Prettier does not like this one
-// const shuffle = halloweenEmojis.sort(() => (Math.random() > 0.5)? 2 : -1);
+let turnedCards = []; // To store the turned cards
+let canClick = true; // To prevent clicking on more than two cards at once
 
-// More concise sorting logic by subtracting 0.5 to get random postive and negative values
+// Shuffle the emojis array
 const shuffle = halloweenEmojis.sort(() => Math.random() - 0.5);
 
 // Card Creation
@@ -46,43 +19,40 @@ for (let i = 0; i < halloweenEmojis.length; i++) {
   cards.innerHTML = shuffle[i];
 
   cards.onclick = function () {
-    this.classList.add("card-turn");
-    setTimeout(function () {
-      if (document.querySelectorAll(".card-turn").length > 1) {
-        if (
-          document.querySelectorAll(".card-turn")[0].innerHTML ==
-          document.querySelectorAll(".card-turn")[1].innerHTML
-        ) {
-          document
-            .querySelectorAll(".card-turn")[0]
-            .classList.add("card-match");
-          document
-            .querySelectorAll(".card-turn")[1]
-            .classList.add("card-match");
+    // Check if we can click (not already turned two cards)
+    if (canClick) {
+      // Check if the card is not already matched or turned
+      if (!this.classList.contains("card-match") && !this.classList.contains("card-turn")) {
+        this.classList.add("card-turn");
+        turnedCards.push(this);
 
-          document
-            .querySelectorAll(".card-turn")[1]
-            .classList.remove("card-turn");
-          document
-            .querySelectorAll(".card-turn")[0]
-            .classList.remove("card-turn");
+        // Check if we have turned two cards
+        if (turnedCards.length === 2) {
+          canClick = false; // Disable further clicks until we process the cards
 
-          if (
-            document.querySelectorAll(".card-match").length ==
-            halloweenEmojis.length
-          ) {
-            alert("win");
+          // Check if the two turned cards match
+          if (turnedCards[0].innerHTML === turnedCards[1].innerHTML) {
+            // If they match, add the "card-match" class to both cards
+            turnedCards[0].classList.add("card-match");
+            turnedCards[1].classList.add("card-match");
+            turnedCards = []; // Reset the turned cards array
+
+            // Check if all cards are matched to determine a win
+            if (document.querySelectorAll(".card-match").length === halloweenEmojis.length) {
+              alert("You win!");
+            }
+          } else {
+            // If they don't match, flip them back down after a delay
+            setTimeout(function () {
+              turnedCards[0].classList.remove("card-turn");
+              turnedCards[1].classList.remove("card-turn");
+              turnedCards = []; // Reset the turned cards array
+              canClick = true; // Enable clicking again
+            }, 500);
           }
-        } else {
-          document
-            .querySelectorAll(".card-turn")[1]
-            .classList.remove("card-turn");
-          document
-            .querySelectorAll(".card-turn")[0]
-            .classList.remove("card-turn");
         }
       }
-    }, 500);
+    }
   };
 
   document.querySelector(".card-game").appendChild(cards);
