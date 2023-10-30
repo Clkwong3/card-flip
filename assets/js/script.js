9b8affc7bf9f13d1e4f1b44214cea237430849b3
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-10-30 03:13:56 -0700

    HTML/JS - add a timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 // Get the "Reset Game" button element from the HTML
 const resetButton = document.querySelector(".reset");
 
+// Get the game board container
+const gameBoard = document.querySelector(".card-game");
+
 // Define an array of Halloween-themed emojis
 const halloweenEmojis = [
+  // An array of Halloween emojis, each repeated twice for matching pairs
   "🎃", "🎃", "👻", "👻", "🦇", "🦇", "🕷️", "🕷️", "🕸️", "🕸️", "🍬", "🍬",
   "🕯️", "🕯️", "🧙‍♀️", "🧙‍♀️", "🧛‍♂️", "🧛‍♂️", "🧟‍♂️", "🧟‍♂️", "🦴", "🦴",
   "🎭", "🎭", "🍁", "🍁", "🌙", "🌙"
@@ -12,16 +16,30 @@ const halloweenEmojis = [
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    [array[i], array[j]] = [array[j], array[i]]; // Swap elements randomly
   }
 }
 shuffleArray(halloweenEmojis);
 
+// Timer variables
+let timer = 0;
+let timerInterval;
+
+// Function to start the timer
+function startTimer() {
+  timerInterval = setInterval(function () {
+    timer++;
+    // Display the timer value in the "timer" element
+    const timerElement = document.getElementById("timer");
+    timerElement.textContent = `Time: ${timer} seconds`;
+  }, 1000); // Update the timer every second (1000 milliseconds)
+}
+
 // Create and display cards with emojis
 for (let i = 0; i < halloweenEmojis.length; i++) {
   const card = document.createElement("div");
-  card.classList.add("emoji-card");
-  card.innerHTML = halloweenEmojis[i];
+  card.classList.add("emoji-card"); // Add the class to style it
+  card.innerHTML = halloweenEmojis[i]; // Display an emoji
 
   // Add a click event listener to the card
   card.addEventListener("click", () => {
@@ -31,7 +49,7 @@ for (let i = 0; i < halloweenEmojis.length; i++) {
       return;
     }
 
-    // Add the "card-turn" class to the clicked card
+    // Add the "card-turn" class to the clicked card to flip it
     card.classList.add("card-turn");
 
     setTimeout(function () {
@@ -39,7 +57,7 @@ for (let i = 0; i < halloweenEmojis.length; i++) {
       if (document.querySelectorAll(".card-turn").length === 2) {
         // Check if the two turned cards match
         if (document.querySelectorAll(".card-turn")[0].innerHTML === document.querySelectorAll(".card-turn")[1].innerHTML) {
-          // Mark them as matched
+          // Mark them as matched by adding "card-match" class
           document.querySelectorAll(".card-turn")[0].classList.add("card-match");
           document.querySelectorAll(".card-turn")[1].classList.add("card-match");
 
@@ -50,19 +68,23 @@ for (let i = 0; i < halloweenEmojis.length; i++) {
           // Check if all cards are matched, then display a win alert
           if (document.querySelectorAll(".card-match").length === halloweenEmojis.length) {
             alert("You win!");
+            clearInterval(timerInterval); // Stop the timer
           }
         } else {
-          // If the cards don't match, turn them back
+          // If the cards don't match, turn them back by removing "card-turn"
           document.querySelectorAll(".card-turn")[1].classList.remove("card-turn");
           document.querySelectorAll(".card-turn")[0].classList.remove("card-turn");
         }
       }
-    }, 800);
+    }, 800); // Delay to allow for card flipping animation
   });
 
   // Append the card to the game container
-  document.querySelector(".card-game").appendChild(card);
+  gameBoard.appendChild(card);
 }
+
+// Start the timer when the game begins
+startTimer();
 
 // Add an event listener for the "Reset Game" button
 resetButton.addEventListener("click", () => {
